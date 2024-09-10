e71b4e00c7992f92dd85df5690bcf8f88f04dce8
MarcosBenegas47 <marcobenega@gmail.com>
2024-09-10 05:33:18 -0300

    archivo configuraicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,8 @@ import styles from "./page.module.css";
 import { getPost } from "./service/randomNumberApi";
 import { useState } from "react";
 import Confetti from "react-dom-confetti";
+import { config } from "@/config";
 
-const config = {
-  position: 'fixed', pointerEvents: 'none',
-  angle: 90,
-  spread: 360,
-  startVelocity: 40,
-  elementCount: 143,
-  dragFriction: 0.12,
-  duration: 3000,
-  stagger: 3,
-  width: "10px",
-  height: "10px",
-  perspective: "500px",
-  colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"]
-}
 
 export default function Home() {
   const [num, setNum] = useState(null);
---
+++ src/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+    position: 'fixed', pointerEvents: 'none',
+    angle: 90,
+    spread: 360,
+    startVelocity: 40,
+    elementCount: 143,
+    dragFriction: 0.12,
+    duration: 3000,
+    stagger: 3,
+    width: "10px",
+    height: "10px",
+    perspective: "500px",
+    colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"]
+  }
